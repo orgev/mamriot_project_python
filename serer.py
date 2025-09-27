d2361ad1d9b8ddb85a1eec7f8417fc56d4d97e62
orgev <78729640+orgev@users.noreply.github.com>
2025-09-27 08:50:44 +0300

    Update serer.py

--- serer.py
+++ serer.py
@@ -1,1 +1,17 @@
+class RemoteConrolServer:
+    #listens to requessts
+    def  __init__(self,ip,port):
+        self.ip=ip
+        self.port=port
+    @staticmethod
+    def echo(request_info):
+        print(request_info)
+    @staticmethod
+    def handle_requests(request_name, request_info):
+        match request_name:
+            case echo:
+                echo(request_info)
+            case _:
+                result="unknown"
+
 
